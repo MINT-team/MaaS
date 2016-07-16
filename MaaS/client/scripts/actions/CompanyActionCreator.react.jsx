5ac346967709ee9c2630f483dc673afec0585ca5
Navid Taha <navidtaha@gmail.com>
2016-07-16 08:53:10 +0000

    add

---
+++ MaaS/client/scripts/actions/CompanyActionCreator.react.jsx
@@ -0,0 +1,13 @@
+var Dispatcher = require("../dispatcher/Dispatcher.js");
+var WebAPIUtils = require("../utils/CompanyWebAPIUtils.js");
+var Constants = require("../constants/Constants.js");
+
+var ActionTypes = Constants.ActionTypes;
+
+var CompanyActionCreator = {
+    getUsers: function(id) {
+        WebAPIUtils.getUsers(id);
+    }
+};
+
+module.exports = CompanyActionCreator;
--- MaaS/client/scripts/components/Footer.react.jsx
+++ MaaS/client/scripts/components/Footer.react.jsx
@@ -4,59 +4,33 @@ var Link = require('react-router').Link;
 var Footer = React.createClass({
     render: function() {
         return (
-        	<footer className="footer-distributed">
-
-			<div className="footer-left">
-
-				<h3>Company<span>logo</span></h3>
-
-				<p className="footer-links">
-					<a href="#">Home</a>
-					·
-					<a href="#">Blog</a>
-					·
-					<a href="#">Pricing</a>
-					·
-					<a href="#">About</a>
-					·
-					<a href="#">Faq</a>
-					·
-					<a href="#">Contact</a>
-				</p>
-
-				<p className="footer-company-name">Company Name &copy; 2015</p>
-			</div>
+        	<div id="footer">
+				<div className="footer-left">
+					<img src="../../images/RedBabelLogo.png" alt="RedBabel Logo" height="40"
+	width="210"/>
+					<p className="footer-links">
+						<a target="_black" href="http://redbabel.com">Home</a>
+						·
+						<a href="#">Contact</a>
+					</p>
+				</div>
 
 			<div className="footer-center">
-
 				<div>
-					<i className="fa fa-map-marker"></i>
-					<p><span>21 Revolution Street</span> Paris, France</p>
+					<p>Maas is offered by RedBabel and developed by MINT with love.</p>
 				</div>
-
-				<div>
-					<i className="fa fa-phone"></i>
-					<p>+1 555 123456</p>
-				</div>
-
-				<div>
-					<i className="fa fa-envelope"></i>
-					<p><a href="mailto:support@company.com">support@company.com</a></p>
-				</div>
-
 			</div>
 
 			<div className="footer-right">
-
-				<p className="footer-company-about">
-					<span>About the company</span>
-					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				<img src="../../images/mint_logo.png" alt="MINT Logo" height="90"
+	width="200"/>
+				<p className="footer-links">
+				<a target="_black" href="https://github.com/MINT-team/">Home</a>
+				·
+				<a href="#">Contact</a>
 				</p>
-
-
 			</div>
-
-		</footer>
+	</div>
 
             );
     }
